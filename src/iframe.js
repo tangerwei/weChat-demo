9006b5230d007055527c3d6d49a537cd90c0dcf1
tangerwei <tangerwei@foxmail.com>
2017-03-16 18:14:23 +0800

    Init

--- src/iframe.js
+++ src/iframe.js
@@ -10,18 +10,41 @@ var setFont = function (w, d) {//w : window d : document
         doc.style.fontSize = 100 * (wd / 640) + "px";
     }
 }
+if (typeof $ == undefined) {
+    var $ = jQuery;
+} else {
+    var _$ = $;
+    $ = jQuery;
+}
 window.onload = function () {
     setFont(window, document);
-    var mySwiper = new Swiper('.swiper-container', {
+    var mySwiper = new Swiper('#swiper-container', {
         autoplay: 5000,//可选选项，自动滑动
     });
-    if(typeof $ == undefined){
-        var $ = jQuery;
-    }else{
-        var _$ = $;
-        $ = jQuery;
-    }
-    $("#nav button").on("click",function(){
-        alert("s");
-    })
+    $("#nav button").on("click", function (e) {
+        if ($(this).hasClass("btn-primary")) {
+            return;
+        } else {
+            $("#nav button").removeClass("btn-default").removeClass("btn-primary").addClass("btn-default");
+            $(this).removeClass("btn-default").addClass("btn-primary");
+            switchNav($(this).attr("data-key"));
+        }
+    });
+    $("#input-group-search").on("click",function(e){
+        searchFruit($(this).parent().prev().val());
+    });
+    var mySwiper2 = new Swiper('#swiper-container-2', {
+        autoplay: 5000,//可选选项，自动滑动
+        pagination : '.swiper-pagination',
+        paginationElement : 'li',
+    });
+}
+//切换显示部分
+function switchNav(key) {
+    $("body section").hide()
+    $(".details-"+key).show();
+}
+
+function searchFruit(keyword){
+    alert(keyword);
 }
