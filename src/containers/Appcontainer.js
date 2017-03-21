45481e403870395d2a262da147db853f8be2f4f6
tangerwei <tangerwei@foxmail.com>
2017-03-21 16:08:08 +0800

    update

--- src/containers/Appcontainer.js
+++ src/containers/Appcontainer.js
@@ -3,29 +3,17 @@
 import AppView from '../views/Appview';
 import {Container} from 'flux/utils';
 import TodoActions from '../data/TodoActions';
-import TodoDraftStore from '../data/TodoDraftStore';
-import TodoEditStore from '../data/TodoEditStore';
-import TodoStore from '../data/TodoStore';
+import ShowingStore from '../data/ShowingStore'
 
 function getStores(){
-    return[TodoEditStore,TodoDraftStore,TodoStore]
+    return[ShowingStore]
 }
 
 function getState(){
     return {
-        draft:TodoDraftStore.getState(),
-        editing:TodoEditStore.getState(),
-        todos:TodoStore.getState(),
+        showing:ShowingStore.getState(),
 
-        onAdd:TodoActions.addTodo,
-        onDeleteCompletedTodos:TodoActions.deleteCompletedTodos,
-        onDeleteTodo:TodoActions.deleteTodo,
-        onEditTodo:TodoActions.editTodo,
-        onStartEditingTodo:TodoActions.startEditingTodo,
-        onStopEditingTodo:TodoActions.stopEditingTodo,
-        onToggleAllTodos:TodoActions.toggleAllTodos,
-        onToggleTodo:TodoActions.toggleTodo,
-        onUpdateDraft:TodoActions.updateDraft
+        onToggleIndex:TodoActions.toggleIndex,
     }
 }
 
