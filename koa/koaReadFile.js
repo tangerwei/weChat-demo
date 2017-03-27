2cd2f7fce3ba15525dc238326761bbe1eb88b51a
tangerwei <tangerwei@foxmail.com>
2017-03-27 16:30:25 +0800

    modify the type of read    file to async

---
+++ koa/koaReadFile.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const _readFileP = function(fileurl){
+    return new Promise(function(resolve,reject){
+        fs.readFile(fileurl,function(err,data){
+            if(err){
+                reject(err)
+            }else{
+                resolve(data);
+            }
+        })
+    });
+}
+const _readFile = async function(url){
+    let data = await _readFileP(url);
+    return data;
+}
+module.exports = {
+    readFile:_readFile
+}
